9b1274e0d74749e678057638c80c54244d0b330f
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-07-31 13:29:14 +0600

    [done]home page hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
+import Banner from "@/components/Banner";
 import RootLayout from "@/components/Layouts/RootLayouts";
+import { Calendar } from "antd";
 
 export default function Home() {
-  return <h1>home page</h1>;
+  return (
+    <div className="max-w-[80vw] mx-auto">
+      <Banner />;<h1>home page</h1>;
+    </div>
+  );
 }
 
 Home.getLayout = function getLayout(page) {
