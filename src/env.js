01a4a6d6d0fc33adf5ac6961c899e3a99442db03
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-08-01 18:52:11 +0600

    [done] backend url added

--- src/env.js
+++ src/env.js
@@ -1,4 +1,5 @@
 export const env = {
-  BASE_URL: "http://localhost:5000",
+  // BASE_URL: "http://localhost:5000",
+  BASE_URL: "https://dream-build.onrender.com",
   Frontend_base_url: "http://localhost:3000",
 };
