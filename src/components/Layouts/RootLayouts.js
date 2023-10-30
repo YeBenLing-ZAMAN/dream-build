07a8a14e37c2b98fab3e062840baca8ca51255e1
md kamruzzaman <kamruzzamanmd@hotmail.com>
2023-10-30 17:52:05 +0600

    toploader added

--- src/components/Layouts/RootLayouts.js
+++ src/components/Layouts/RootLayouts.js
@@ -1,9 +1,11 @@
 import Footer from "../Footer/Footer";
+import TopLoader from "../Loader/NextTopLoader";
 import Navbar from "../navbar/Navbar";
 
 export default function RootLayout({ children }) {
   return (
     <div>
+      <TopLoader />
       <Navbar></Navbar>
       {children}
       <Footer></Footer>
